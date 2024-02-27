62a5f4619fcfb847e00b66e3df494418f1c352c3
typeAl_ <161527750+typeAl@users.noreply.github.com>
2024-02-27 17:51:52 -0500

    Create CombatHandler.lua

---
+++ ServerStorage/Modules/Loader/CombatHandler.lua
@@ -0,0 +1,33 @@
+--[Never intended to be completed. Users need to add their own logic into this module.]--
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Remotes = ReplicatedStorage:WaitForChild("Remotes")
+
+local Client = Remotes:WaitForChild("Client")
+
+local CombatHandler = {
+	["Light"] = function(Player, Args)
+		Args = "Light Server"
+		print(Args)
+		
+		Client:FireAllClients({
+			Module = "CombatVFX",
+			Function = "LightHit",
+			Information = "Hit Client"
+		})
+	end,
+	
+	["Heavy"] = function(Player, Args)
+		Args = "Heavy Server"
+		print(Args)
+		
+		Client:FireAllClients({
+			Module = "CombatVFX",
+			Function = "HeavyHit",
+			Information = "Hit Client"
+		})
+	end,
+}
+
+return CombatHandler
